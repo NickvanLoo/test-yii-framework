45a851e1c8a0b816da2b8ac40b6f998654c2d5ad
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-08-01 21:41:34 +0200

    test-yii2-framework

---
+++ views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My first Yii Application';
+?>
+<div class="site-index">
+<p> Dit is mijn 1e yii2 applicatie, om het Framework een beetje te leren kennen.</p>
+            </div>
+        </div>
+
+    </div>
+</div>
